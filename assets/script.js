80ed4b185984d46f16ee6946d52976836e4a5dac
kingma-sbw <kingma@sbw-media.ch>
2022-01-31 18:42:35 +0100

    use await

--- assets/script.js
+++ assets/script.js
@@ -2,41 +2,45 @@
 const clientId = 'ca000ee4f7eb40b993c71aaeb3bc3813';
 const clientSecret = '9063ae6add144f209f9c44135da9b92a';
 
-let okBtn = document.getElementById('ok').onclick = function() {
-  const fetchOptions = {
-    method: 'POST',
-    headers: {
+let okBtn = document.getElementById('ok').onclick = async function() {
+  const headers =  {
       'Accept': 'application/json',
       'Content-Type':  'application/x-www-form-urlencoded',
       'Authorization': 'Basic ' + window.btoa(clientId + ':' + clientSecret),
-    },
-    body: "grant_type=client_credentials",
   };
+  const body = "grant_type=client_credentials";
 
-  fetch( 'https://accounts.spotify.com/api/token', fetchOptions )
-    .then(response => response.json() )
-    .then(response => response.access_token)
-    .then(getTracks);
+  const url = 'https://accounts.spotify.com/api/token';
+  const options = { method: 'POST', headers: headers, body: body };
 
+  const response = await fetch( url, options );
+  if( response.status >= 200 && response.status <= 204 ) {
+    let data = await response.json();
+    getTracks(data.access_token)
+  } else {
+    console.log('Intruder alert '+response.status);
+  }
 
-
-  function getTracks(token) {
+  async function getTracks(token) {
     let interpret = document.getElementById('interpret').value;
     let album = document.getElementById('album').value;
     const url = "https://api.spotify.com/v1/search/"
     + '?type=track'
     + `&q=${album}+artist:${interpret}`
 
-    const fetchOptions = {
-      method: 'GET',
-      headers: {
+    const headers = {
         'Accept': 'application/json',
-        'Authorization': 'Bearer ' + token,
-      },
+        //'Authorization': 'Bearer ' + token,
     };
-    fetch(url, fetchOptions)
-      .then(result => result.json())
-      .then(showResult)
+    const options = { method: 'GET', headers: headers };
+
+    const response = await fetch(url, options);
+    if( response.status >= 200 && response.status <= 204 ) {
+      let data = await response.json();
+      showResult(data);
+    } else {
+      console.log('Danger '+ response.status)
+    }
   }
   function showResult(data) {
     let table = document.createElement('ul');
